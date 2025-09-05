82b4eb01450d47d3baf266dadefdbbb799897b96
cheliszz <teixeiramicheli96@gmail.com>
2025-09-05 16:12:09 -0300

    Create script.js

---
+++ img/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+ const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+ const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+
+let tamanhoAtualFonte = 1;
